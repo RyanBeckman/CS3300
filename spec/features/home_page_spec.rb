52933276ce97358a1f113936a6f2074e159267b3
root <rbeckman@uccs.edu>
2022-05-04 06:47:35 +0000

    Updated formating and tests

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -10,6 +10,6 @@ RSpec.feature "Visiting the homepage", type: :feature do
   scenario "The visitor should see projects" do
     newAdmin
     visit root_path
-    expect(page).to have_text("Projects")
+    expect(page).to have_text("Portfolio")
   end
 end
